4041e302bf63ff8519b9f778da7f9af102197d02
Freddy Chiron <fchiron.ext@orange.com>
2016-02-18 15:16:07 +0100

    add tests with properties values comparison

--- Java/src/main/java/com/gildedrose/TexttestFixture.java
+++ Java/src/main/java/com/gildedrose/TexttestFixture.java
@@ -4,19 +4,19 @@ public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        ItemLegacy[] items = new ItemLegacy[] {
+                new ItemLegacy("+5 Dexterity Vest", 10, 20), //
+                new ItemLegacy("Aged Brie", 2, 0), //
+                new ItemLegacy("Elixir of the Mongoose", 5, 7), //
+                new ItemLegacy("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new ItemLegacy("Sulfuras, Hand of Ragnaros", -1, 80),
+                new ItemLegacy("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new ItemLegacy("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new ItemLegacy("Backstage passes to a TAFKAL80ETC concert", 5, 49),
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                new ItemLegacy("Conjured Mana Cake", 3, 6) };
 
-        GildedRose app = new GildedRose(items);
+        GildedRoseLegacy app = new GildedRoseLegacy(items);
 
         int days = 2;
         if (args.length > 0) {
@@ -26,7 +26,7 @@ public class TexttestFixture {
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
-            for (Item item : items) {
+            for (ItemLegacy item : items) {
                 System.out.println(item);
             }
             System.out.println();
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,66 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
 public class GildedRoseTest {
 
-    @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-    }
+	@Test
+	public void should_return_same_results_between_refactored_and_legacy_versions() {
+		ItemLegacy[] itemsLegacy = new ItemLegacy[] { new ItemLegacy("+5 Dexterity Vest", 10, 20), //
+				new ItemLegacy("Aged Brie", 2, 0), //
+				new ItemLegacy("Elixir of the Mongoose", 5, 7), //
+				new ItemLegacy("Sulfuras, Hand of Ragnaros", 0, 80), //
+				new ItemLegacy("Sulfuras, Hand of Ragnaros", -1, 80),
+				new ItemLegacy("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+				new ItemLegacy("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+				new ItemLegacy("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+				// this conjured item does not work properly yet
+				new ItemLegacy("Conjured Mana Cake", 3, 6) };
+
+		GildedRoseLegacy legacyApp = new GildedRoseLegacy(itemsLegacy);
+
+		int days = 2;
+
+		StringBuilder legacyResult = new StringBuilder();
+		for (int i = 0; i < days; i++) {
+			legacyResult.append("-------- day " + i + " --------");
+			legacyResult.append("name, sellIn, quality");
+			for (ItemLegacy item : itemsLegacy) {
+				legacyResult.append(item);
+			}
+			legacyResult.append("\n");
+			legacyApp.updateQuality();
+		}
+
+		Item[] items = new Item[] { new Item("+5 Dexterity Vest", 10, 20), //
+				new Item("Aged Brie", 2, 0), //
+				new Item("Elixir of the Mongoose", 5, 7), //
+				new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+				new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+				// this conjured item does not work properly yet
+				new Item("Conjured Mana Cake", 3, 6) };
+
+		GildedRose app = new GildedRose(items);
+
+		days = 2;
+
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < days; i++) {
+			result.append("-------- day " + i + " --------");
+			result.append("name, sellIn, quality");
+			for (Item item : items) {
+				result.append(item);
+			}
+			result.append("\n");
+			app.updateQuality();
+		}
+
+		Assert.assertEquals(legacyResult.toString(), result.toString());
+
+	}
 
 }
