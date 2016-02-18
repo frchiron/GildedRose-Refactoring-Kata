116dbf82a519e9e82d5c9157db968329194f5fc4
Freddy Chiron <fchiron.ext@orange.com>
2016-02-18 17:12:05 +0100

    additional refactor part 5

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,8 +1,8 @@
 package com.gildedrose;
 
-import java.util.Arrays;
-
 class GildedRose {
+	private static final int FIVE_DAYS = 5;
+	private static final int TEN_DAYS = 10;
 	private static final int QUALITY_MIN = 0;
 	private static final int QUALITY_MAX = 50;
 	private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
@@ -16,72 +16,91 @@ class GildedRose {
 
 	public void updateQuality() {
 		for (Item item : items) {
-
 			decrementSellIn(item);
-
 			updateQuality(item);
-
-			if (item.sellIn < 0) {
-				updateQualityWhenNegativeSellIn(item);
-			}
 		}
 
 	}
 
 	public void decrementSellIn(Item item) {
-		if (!item.name.equals(SULFURAS)) {
+		if (!SULFURAS.equals(item.name)) {
 			item.sellIn = item.sellIn - 1;
 		}
 	}
 
 	public void updateQuality(Item item) {
-		if (!Arrays.asList(AGED_BRIE, BACKSTAGE_PASSES, SULFURAS).contains(item.name)) {
+		updateQualityFirstPart(item);
+
+	}
+
+	public void updateQualityFirstPart(Item item) {
+		switch (item.name) {
+		case AGED_BRIE:
+			updateQualityForAgedBrie(item);
+			break;
+		case SULFURAS:
+			updateQualityForSulfuras(item);
+			break;
+		case BACKSTAGE_PASSES:
+			updateQualityForBackstagePasses(item);
+			break;
+		default:
+			updateQualityDefault(item);
+			break;
+		}
+	}
+
+	public void updateQualityDefault(Item item) {
+		if (item.quality > QUALITY_MIN) {
+			decrementQualityByOne(item);
+		}
+
+		if (item.sellIn < 0) {
 			if (item.quality > QUALITY_MIN) {
 				decrementQualityByOne(item);
 			}
-			return;
+		}
+	}
+
+	public void updateQualityForBackstagePasses(Item item) {
+		if (item.quality < QUALITY_MAX) {
+			incrementQualityByOne(item);
+
+		}
+		if (item.quality < QUALITY_MAX) {
+			if (item.sellIn < TEN_DAYS) {
+				incrementQualityByOne(item);
+			}
+
+			if (item.sellIn < FIVE_DAYS) {
+				incrementQualityByOne(item);
+			}
 		}
 
+		if (item.sellIn < 0) {
+			item.quality = QUALITY_MIN;
+		}
+	}
+
+	public void updateQualityForAgedBrie(Item item) {
 		if (item.quality < QUALITY_MAX) {
 			incrementQualityByOne(item);
 
 		}
 
-		if (item.name.equals(AGED_BRIE) || item.name.equals(SULFURAS)) {
-			return;
-		}
-
-		if (item.sellIn < 10) {
+		if (item.sellIn < 0) {
 			if (item.quality < QUALITY_MAX) {
 				incrementQualityByOne(item);
 			}
-		}
 
-		if (item.sellIn < 5) {
-			if (item.quality < QUALITY_MAX) {
-				incrementQualityByOne(item);
-			}
 		}
-
 	}
 
-	public void updateQualityWhenNegativeSellIn(Item item) {
-		if (item.name.equals(AGED_BRIE)) {
-			if (item.quality < QUALITY_MAX) {
-				incrementQualityByOne(item);
-			}
-			return;
+	public void updateQualityForSulfuras(Item item) {
+		if (item.quality < QUALITY_MAX) {
+			incrementQualityByOne(item);
 		}
-		if (item.name.equals(BACKSTAGE_PASSES)) {
-			item.quality = QUALITY_MIN;
-			return;
-		}
-		if (!item.name.equals(SULFURAS)) {
-			if (item.quality > QUALITY_MIN) {
-				decrementQualityByOne(item);
-			}
-			return;
-		}
+
 	}
 
 	public int decrementQualityByOne(Item item) {
