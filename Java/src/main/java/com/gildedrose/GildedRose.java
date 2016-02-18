9483139d3b378f97c0d3b5262c74bc85600b467d
Freddy Chiron <fchiron.ext@orange.com>
2016-02-18 15:49:56 +0100

    add methods for updateQuality

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,52 +14,62 @@ class GildedRose {
 		for (int i = 0; i < items.length; i++) {
 
 			Item item = items[i];
-			if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
-				if (item.quality > 0) {
-					if (!item.name.equals(SULFURAS)) {
-						item.quality = item.quality - 1;
-					}
-				}
-			} else {
-				if (item.quality < 50) {
-					item.quality = item.quality + 1;
-
-					if (item.name.equals(BACKSTAGE_PASSES)) {
-						if (item.sellIn < 11) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-
-						if (item.sellIn < 6) {
-							if (item.quality < 50) {
-								item.quality = item.quality + 1;
-							}
-						}
-					}
-				}
-			}
-
 			decrementSellIn(item);
 
+			updateQuality(item);
 			if (item.sellIn < 0) {
-				if (item.name.equals(AGED_BRIE)) {
-					if (item.quality < 50) {
-						item.quality = item.quality + 1;
-					}
+				updateQualityWhenNegativeSellIn(item);
+			}
+		}
+
+	}
+
+	public void updateQuality(Item item) {
+		if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
+			if (item.quality > 0) {
+				if (!item.name.equals(SULFURAS)) {
+					item.quality = item.quality - 1;
 				}
+			}
+		} else {
+			if (item.quality < 50) {
+				item.quality = item.quality + 1;
+
 				if (item.name.equals(BACKSTAGE_PASSES)) {
-					item.quality = 0;
-				}
-				if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)
-						&& !item.name.equals(SULFURAS)) {
-					if (item.quality > 0) {
-						item.quality = item.quality - 1;
+					if (item.sellIn < 10) {
+						if (item.quality < 50) {
+							item.quality = item.quality + 1;
+						}
+					}
+
+					if (item.sellIn < 5) {
+						if (item.quality < 50) {
+							item.quality = item.quality + 1;
+						}
 					}
 				}
 			}
 		}
 
+	}
+
+	public void updateQualityWhenNegativeSellIn(Item item) {
+		if (item.name.equals(AGED_BRIE)) {
+			if (item.quality < 50) {
+				item.quality = item.quality + 1;
+			}
+			return;
+		}
+		if (item.name.equals(BACKSTAGE_PASSES)) {
+			item.quality = 0;
+			return;
+		}
+		if (!item.name.equals(SULFURAS)) {
+			if (item.quality > 0) {
+				item.quality = item.quality - 1;
+			}
+			return;
+		}
 	}
 
 	public void decrementSellIn(Item item) {
